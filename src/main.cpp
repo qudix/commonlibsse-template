ed298663afa7d645b418159a36a04cd0ca457a6b
Qudix <17361645+Qudix@users.noreply.github.com>
2024-09-07 00:11:20 -0500

    feat: update

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,8 @@
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/msvc_sink.h>
-
-void InitLogging()
+SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
-	auto path = logs::log_directory();
-	if (!path)
-		return;
+	SKSE::Init(a_skse, true);
 
-	*path /= "commonlibsse-template.log";
-
-	spdlog::sinks_init_list sinks{
-		std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true),
-		std::make_shared<spdlog::sinks::msvc_sink_mt>()
-	};
-
-	auto logger = std::make_shared<spdlog::logger>("global", sinks);
-	logger->set_level(spdlog::level::info);
-	logger->flush_on(spdlog::level::info);
-
-	spdlog::set_default_logger(std::move(logger));
-	spdlog::set_pattern("[%^%L%$] %v");
-}
-
-extern "C" __declspec(dllexport)
-bool SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
-{
-	InitLogging();
-
-	logs::info("commonlibsse-template is loading...");
-
-	SKSE::Init(a_skse);
-
-	logs::info("commonlibsse-template loaded.");
+	logs::info("Hello World!");
 
 	return true;
 }
